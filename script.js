7c077955c6ab0abc4ef6ea133f376aca6708feb1
olivia-hawkins <dg06fnyy@gmail.com>
2025-05-09 08:16:20 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 document.querySelectorAll("button").forEach(button => {
-    // スマホのタッチ押下時にクラス追加
     button.addEventListener("touchstart", () => {
       button.classList.add("pressed");
     });
   
-    // タッチ解除・キャンセル時にクラス削除
     const removePressed = () => button.classList.remove("pressed");
   
     button.addEventListener("touchend", removePressed);
